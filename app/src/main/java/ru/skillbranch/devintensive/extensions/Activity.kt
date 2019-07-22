1daf172c3baa745e2b291b999c1ff415c637f2b3
AlexanderPokidin <a.pokidin@gmail.com>
2019-07-22 16:04:58 +0300

    Initial commit

---
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
@@ -0,0 +1,14 @@
+package ru.skillbranch.devintensive.extensions
+
+import android.app.Activity
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+
+fun Activity.hideKeyboard() {
+    val inputMethodManager: InputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+    val view: View? = this.currentFocus
+    if (view != null) {
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+}
+
