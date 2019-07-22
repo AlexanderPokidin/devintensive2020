679aaf9d8d8641ee0486678ec75b350958b9cb8d
AlexanderPokidin <a.pokidin@gmail.com>
2019-07-22 16:50:56 +0300

    Last Fixed

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
@@ -1,6 +1,9 @@
 package ru.skillbranch.devintensive.extensions
 
 import android.app.Activity
+import android.content.Context
+import android.graphics.Rect
+import android.util.TypedValue
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 
@@ -12,3 +15,19 @@ fun Activity.hideKeyboard() {
     }
 }
 
+fun Activity.isKeyboardOpen(): Boolean {
+    val rootView = findViewById<View>(android.R.id.content)
+    val visibleBorders = Rect()
+    rootView.getWindowVisibleDisplayFrame(visibleBorders)
+    val heightDifference = rootView.height - visibleBorders.height()
+
+    return heightDifference > dpToPx(100F, this)
+}
+
+fun Activity.isKeyboardClosed(): Boolean {
+    return !this.isKeyboardOpen()
+}
+
+fun dpToPx(dp: Float, context: Context): Int {
+    return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, context.resources.displayMetrics).toInt()
+}
